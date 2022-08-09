ea9b257f347f0d0c92bef51f2ec2d78bebccc9dd
liudehua <liudehua@163.com>
2022-08-09 16:29:26 +0800

    add json dump Q

--- package/json.py
+++ package/json.py
@@ -25,3 +25,12 @@ print(dataDict, type(dataDict))
 {"name": "Jack", "age": 32, "vip": true, "address": {"province": "bj", "city": "hd"}} <class 'str'>
 {'name': 'Jack', 'age': 32, 'vip': True, 'address': {'province': 'bj', 'city': 'hd'}} <class 'dict'>
 '''
+
+dict = {'name': "萝卜", 'age': 18}
+dictDump = json.dumps(dict)
+print(dictDump)
+# {"name": "\u841d\u535c", "age": 18}
+
+dictDump1 = json.dumps(dict, ensure_ascii=False)
+print(dictDump1)
+# {"name": "萝卜", "age": 18}
